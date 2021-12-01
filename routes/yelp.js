4c53cbfc8ee4913f09e049d463223ad4f114f569
johns <johns@wfu.edu>
2021-12-01 16:39:01 -0500

    Add more config.

--- routes/yelp.js
+++ routes/yelp.js
@@ -24,40 +24,38 @@ router.post('/', (req,res) => {
     })
     .then(response => {
         console.log(JSON.stringify(response.data.businesses))
+        const realData = (response.data.businesses)
         const dataLen = JSON.stringify(response.data.businesses.length)
         let name
         let image
         let rating
         let address
-        const food = []
+        const foodArr = []
         const foodObj = {}
 
         for (i=0; i<=(dataLen-1); i++){ 
             //console.log(response.data.businesses[i].rating)
             //console.log(response.data.businesses[i].location.address1 +","+ response.data.businesses[i].location.state)
             //console.log(response.data.businesses[i].image_url)
-            name = response.data.businesses[i].name
-            image = response.data.businesses[i].image_url
-            rating = response.data.businesses[i].rating
-            address = [response.data.businesses[i].location.address1 +","+ response.data.businesses[i].location.state]
+            name = realData[i].name
+            image = realData[i].image_url
+            rating = realData[i].rating
+            address = [realData[i].location.address1 +","+ realData[i].location.state]
             foodObj[i] = {
                 name: name,
                 image: image,
                 rating: rating,
                 address: address
             }
-            food.push(foodObj[i])
+            foodArr.push(foodObj[i])
 
         }
-        console.log (food)
+        console.log (foodArr)
 
-        res.render('index', {
-            //name, image, rating, address
-            food
-        })
+        res.render('index', {food:foodArr})
     })
     .catch ((err) => {
-        console.log(err)
+        console.log(err.response.status)
     })
 })
 
